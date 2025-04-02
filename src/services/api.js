e6d4a944e2ff0014c31fb3b826786436a03ea812
ronischiaretti <pnamaquina@gmail.com>
2025-04-02 17:48:16 -0300

    ajustei o frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,17 @@
 import axios from 'axios'
 
 const api = axios.create({
-   baseURL:"https://backendalesandro-production.up.railway.app/",
+   baseURL:"https://backendalesandro-production.up.railway.app/api",  
 
 });
+
+// Adiciona o token automaticamente a cada requisição
+api.interceptors.request.use(config => {
+   const token = localStorage.getItem('token');
+   if (token) {
+     config.headers.Authorization = `Bearer ${token}`;
+   }
+   return config;
+ });
 
 export default api 
